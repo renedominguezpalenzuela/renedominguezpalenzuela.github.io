4b8eb055269ecfcd979c144666f1f962d7f02c66
rdominguez <rdominguez@soaint.com>
2023-05-26 17:37:47 -0400

    login page

--- js/app.js
+++ js/app.js
@@ -4,23 +4,6 @@ const { Component, mount, xml, useState} = owl;
 
 
 
-class MagicButton extends Component {
-  static template = xml`
-    <button t-on-click="changeText">
-      <t t-esc="props.initialText.texto"/> [<t t-esc="state.value"/>]
-    </button>`;
-
-  state = useState({ value: 0 });
-
-  changeText() {
-    this.state.value = "This is Magic";
-  }
-
-   static props = ["initialText"];
-   setup() {
-    console.log("SSS");
-   }
-}
 
 
 // Owl Components
@@ -28,17 +11,41 @@ class Root extends Component {
 
   initialText = {texto: 'Dont click on me.'};
   static template = xml`
-   <div class="p-3 ">
-     <span class="ml-1">
-        todo app hh
-     </span>
-     <div>
-        <MagicButton initialText="initialText"/>
-     </div>
-   </div>
+   
+
+      <div class="main-back grid place-items-center h-screen ">
+        <div class="d-sm-none ">
+          <img src="img/logo.png" alt="" class="img-size"/>
+        </div>
+
+         <div class=" h-[34rem] w-[80vw] sm:w-[62vw] bg-white shadow-lg flex border-rounded ">
+           <div class="h-full w-[90%] sm:w-[45vw] left p-5  ">             
+                <h3>SIGN IN</h3>
+                <input type="text" placeholder="USERNAME" id="usr"/>
+                <input type="password" placeholder="PASSWORD" id="pass"/>                
+                <div class="flex">                 
+                  <button id="test-data-btn">Set Test Data</button>           
+                  <button id="test-api-btn">Test API</button>
+                </div>
+                <button id="login-btn" class="submit-btn">LET'S GO</button>
+               
+           </div>
+
+           <div class="h-full w-[100%] hidden sm:block right p-5 ">
+            <div class="right-text">
+            <h2>Volttus</h2>
+
+            <h5>Lorem, ipsum dolor sit amet consectetur </h5>
+              </div>
+             
+           </div>
+            
+         </div>  
+      </div>
+
   `;
 
-  static components = { MagicButton };
+  //static components = { MagicButton };
 }
 
 mount(Root, document.body);
