08cc8d54735d5c3c9ad1d9585ae45acefcc695f1
rdominguez <rdominguez@soaint.com>
2023-06-09 17:49:30 -0400

    left menu format ok

--- js/components/leftmenu.js
+++ js/components/leftmenu.js
@@ -3,43 +3,89 @@ const { Component , xml} = owl;
 
 
 
+class MenuItemCollapsable extends Component {
+    static  template=xml`
+    <div class="collapse  collapse-arrow ">
+        <input type="checkbox" class="peer" /> 
+        
+        <div class="collapse-title pl-4 ">
+             <t t-esc="props.name"/>
+        </div>
+
+        <div class="collapse-content"> 
+            <div t-foreach="props.items" t-as="unitem" t-key="unitem.id" class="pl-2" >
+                 <t t-esc="unitem.name"/>
+            </div>
+        </div>
+        
+    </div>
+    `;
+}
+
+
+
+/*
+<div class="collapse collapse-arrow ">
+<input type="radio" name="my-accordion-2" checked="checked" /> 
+
+<div class="collapse-title ">
+     <t t-esc="props.name"/>
+</div>
+
+<div class="collapse-content"> 
+    <div t-foreach="props.items" t-as="unitem" t-key="unitem.id" >
+        <t t-esc="unitem.name"/>
+    </div>
+    
+</div>
+</div>
+*/
+
+class MenuItemSingle extends Component {
+    static  template=xml`
+    
+        <div class="pl-4 ">
+             <t t-esc="props.name"/>
+        </div>
+
+        
+    `;
+}
+
+
+
 export class LeftMenu extends Component {
+
+
+    static components = { MenuItemCollapsable, MenuItemSingle };
+
+
     static template=xml`       
-            <div class="bg-[#009AFF] rounded-lg text-white text-[1rem] h-full"> 
-            
-                
+            <div class="bg-[#009AFF] rounded-lg text-white text-[1rem] h-full">            
+
                 <a class="flex items-center justify-center p-3" href="/userdata.html">
                     <img src="../img/logo-white.png" height="16px"  alt="Logo" loading="lazy"  class="img-logo"  />
                 </a>
-                
-                <div class="collapse collapse-arrow ">
-                    <input type="radio" name="my-accordion-2" checked="checked" /> 
-                    <div class="collapse-title text-xl font-medium">
-                        TX List
-                    </div>
-                    <div class="collapse-content"> 
-                        <p>hello</p>
-                    </div>
+
+                <div t-foreach="props.items" t-as="unitem" t-key="unitem.id" >
+                   <t t-if="unitem.type === 1">                       
+                      <MenuItemCollapsable  name="unitem.name" items="unitem.subitems"/>                         
+                   </t>
+                   <t t-if="unitem.type === 2">                       
+                     <MenuItemSingle  name="unitem.name" />                         
+                   </t>
                 </div>
-                <div class="collapse collapse-arrow ">
-                    <input type="radio" name="my-accordion-2" /> 
-                    <div class="collapse-title text-xl font-medium">
-                    Send Money
-                    </div>
-                    <div class="collapse-content"> 
-                    <p>Send Money Cuba</p>
-                    </div>
-                </div>
-                <div class="collapse collapse-arrow ">
-                    <input type="radio" name="my-accordion-2" /> 
-                    <div class="collapse-title text-xl font-medium">
-                    User Balance
-                    </div>
-                    <div class="collapse-content"> 
-                    <p>hello</p>
-                    </div>
-                </div>    
+               
             </div>  
     `;
+
+
+ 
+    setup() {
+       
+    
+       console.log(this.props)
+    }
 }
+
 
--- js/main.js
+++ js/main.js
@@ -6,13 +6,34 @@ import { LeftMenu } from "./components/leftmenu.js";
 
 
 
+
 class Root extends Component {
   static components = { Menu, LeftMenu };
+
+
+  leftmenuItems = [
+    { id: 1, name: "Profile", type: 2 },
+    { id: 2, name: "Receivers", type: 2 },
+    {
+      id: 3, name: "Send Money", type: 1, subitems: [
+        { id: 4, name: "Send Money To Cuba" },
+        { id: 4, name: "Send Money To Mexico" },
+      ]
+    },
+
+    {
+      id: 4, name: "Send Money 2", type: 1, subitems: [
+        { id: 5, name: "Send Money To Cuba 2" },
+        { id: 6, name: "Send Money To Mexico 2" },
+      ]
+    },
+  ];
+
   static template = xml`  
 
     <div class="grid  grid-cols-[15%_85%]   w-full bg-[#F1F2F7] h-screen">
       <div class="p-2">
-       <LeftMenu/>
+       <LeftMenu  items="leftmenuItems"/>
       </div>
       <div class="p-2 h-full ">
       <Menu/>
@@ -37,12 +58,26 @@ class Root extends Component {
     const userId = window.localStorage.getItem('userId');
 
 
-    onWillStart(async () => {
-      
 
 
+
+    onWillStart(() => {
+      console.log("1) WillStart");
     });
+
+    onMounted(() => {
+      console.log("2) Mounted");
+    });
+
+
+    onRendered(() => {
+      console.log("3) Rendered");
+    });
+
   }
+
+
+
 
 }
 
