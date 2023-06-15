ecfeb00806442ca78db61680066a3a1bb645b82a
rdominguez <rdominguez@soaint.com>
2023-06-15 11:17:30 -0400

    estado ok

--- js/components/sendmoney.js
+++ js/components/sendmoney.js
@@ -26,12 +26,12 @@ export class SendMoney extends Component {
     avatar: "/img/photo-1534528741775-53994a69daeb.jpg",
     address: "",
     nameFull: "",
-    /*sendValue: 0,
-    receiveValue: 0,
-    conversionRateSTR: "",
-    conversionRate: 0,
-    feeSTR: "",
-    fee: 0*/
+    // sendValue: 0,
+    // receiveValue: 0,
+    // conversionRateSTR: "",
+    // conversionRate: 0,
+    // feeSTR: "",
+    // fee: 0
   })
 
   conversionRateSTR = useState({value:""});
@@ -48,44 +48,36 @@ export class SendMoney extends Component {
     <div class="grid sm:grid-cols-[34%_64%] gap-2">
       <div class="card  w-full bg-base-100 shadow-xl rounded-lg">
         <div class="card-title flex flex-col rounded-lg">
-           
-
-          
-
           
         </div>
 
-        
-
-
-
         <div class="card-body items-center  ">
-
+           
             <div class="form-control  max-w-xs  ">
                 <label class="label">
                   <span class="label-text">You Send</span>  
                 </label> 
 
                 <div class="join">
-
+                              
                   <div>
                     <input type="number" t-ref="inputSendRef" t-on-input="onChangeSendInput"  t-att-value="state.sendValue" step="0.01" min="-9999999999.99" max="9999999999.99" class="input input-bordered join-item text-right" placeholder="0.00"/>
                     <label class="label">
                       <span class="label-text-alt"></span>
                       <span class="label-text-alt ">
                         <div class=" text-right">
-                          Send Fee: <t t-esc="feeSTR.value"/> 
+                          Send Fee: <t t-esc="this.feeSTR.value"/> 
                         </div>  
                         <div class=" text-right">  
-                           <t t-esc="conversionRateSTR.value"/> 
+                           <t t-esc="this.conversionRateSTR.value"/> 
                         </div>
                       </span>
                     </label>
                   </div>
-
+                 
 
                   <select class="select select-bordered join-item" t-on-input="onChangeCurrencySend" t-ref="inputSendCurrencyRef" >                    
-                    <option t-att-selected="true" value="usd">USD</option>
+                    <option value="usd">USD</option>
                     <option value="eur">EUR</option>
                     <option value="cad">CAD</option>
                     
@@ -95,24 +87,21 @@ export class SendMoney extends Component {
               </div>
 
 
-
+             
               <div class="form-control  max-w-xs   ">
               <label class="label">
                 <span class="label-text">Received Amount</span>  
               </label> 
 
               <div class="join">
-
-                <div>
-              
-                  <input type="number" t-ref="inputReceiveRef" t-on-input="onChangeReceiveInput" t-att-value="state.receiveValue"  step="0.01" min="-9999999999.99" max="9999999999.99" class="input input-bordered join-item text-right" placeholder="0.00"/>
-              
+               
+                <div>           
+                  <input type="number" t-ref="inputReceiveRef" t-on-input="onChangeReceiveInput" t-att-value="state.receiveValue"  step="0.01" min="-9999999999.99" max="9999999999.99" class="input input-bordered join-item text-right" placeholder="0.00"/>             
                 </div>
                 
 
-                <select class="select select-bordered join-item" t-ref="inputReceiveCurrencyRef" t-on-input="onChangeCurrencyRecib" >
-              
-                  <option  t-att-selected="true"  value="cup">CUP</option>
+                <select class="select select-bordered join-item" t-ref="inputReceiveCurrencyRef" t-on-input="onChangeCurrencyRecib" >     
+                  <option value="cup">CUP</option>
                   <option value="usd">USD</option>
                 </select>
 
@@ -164,17 +153,15 @@ export class SendMoney extends Component {
 
 
       this.state = { ...userData };
-      //this.state.conversionRateSTR = '-';
+  
       this.feeSTR.value = '-';
 
       this.conversionRate.value = exchangeRate["CUP"];
-
-      console.log(exchangeRate);
-
-      console.log(this.conversionRate.value);
-
       this.conversionRateSTR.value = `1 USD = ${this.conversionRate.value} CUP`;
 
+      console.log("Will Start");
+      console.log(exchangeRate);
+      console.log(this.conversionRate.value);
       console.log(this.conversionRateSTR.value);
 
 
@@ -232,17 +219,14 @@ export class SendMoney extends Component {
     console.log("Enviar en: " + sendCurrency);
     console.log("Recibir en: " + receiveCurrency);
 
-    if (receiveCurrency && sendCurrency ) {
+  if (receiveCurrency && sendCurrency ) {
 
       const exchangeRate = await api.getExchangeRate(sendCurrency);
       this.conversionRate.value = exchangeRate[receiveCurrency.toUpperCase()];
-
-      console.log(exchangeRate);
-
-      console.log(this.conversionRate.value);
-
       this.conversionRateSTR.value = `1 ${sendCurrency.toUpperCase()} = ${this.conversionRate.value} ${receiveCurrency.toUpperCase()}`;
 
+      console.log(exchangeRate);
+      console.log(this.conversionRate.value);
       console.log(this.conversionRateSTR.value);
     }
 
